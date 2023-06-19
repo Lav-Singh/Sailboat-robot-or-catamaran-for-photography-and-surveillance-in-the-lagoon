c346fca52ca279c0982053e9471009c6815e06e8
Lav-Singh <44323038+Lav-Singh@users.noreply.github.com>
2023-06-19 13:26:16 +0400

    Update utilities.py

--- wamv_control/src/Tools/utilities.py
+++ wamv_control/src/Tools/utilities.py
@@ -46,27 +46,6 @@ def normalize_angle(theta):
     theta_norm = (theta + math.pi) % (2 * math.pi) - math.pi
     return theta_norm
 
-def angle_within_half_plane_range(theta_check, theta_min, theta_max):
-    ''''
-    Check whether an angle lies within certain half-plane range, all in [-pi, pi).
-    Note: Angle on the range edge is not deemed as within it.
-
-    :param theta_check: Angle to be checked in radians in [-pi, pi)
-    :param theta_min  : Minimum angle in radians in [-pi, pi)
-    :param theta_max  : Maximum angle in radians in [-pi, pi)
-
-    :return result: Boolean flag indicating whether `theta_check` lies within `theta_min` and `theta_max`
-    '''
-    assert abs(normalize_angle(theta_min - theta_max)) <= math.pi
-    if theta_min == theta_max:
-        return False
-    if theta_check == theta_min or theta_check == theta_max:
-        return False
-    cur_to_min = normalize_angle(theta_check - theta_min)
-    cur_to_max = normalize_angle(theta_check - theta_max)
-    cur_quad, start_quad, end_quad = quadrant(theta_check), quadrant(theta_min), quadrant(theta_max)
-    result = ((cur_to_min > 0) ^ (cur_to_max > 0)) and ((cur_quad == start_quad) | (cur_quad == end_quad))
-    return result
 
 def quadrant(theta):
     '''
@@ -163,59 +142,3 @@ def enu_to_gps(x, y, z=0):
     lon = gps[1]
     alt = gps[2]
     return lat, lon, alt
-
-def heading_to_bearing(heading):
-    '''
-    Convert given heading in radians to compass bearing in degrees.
-    Note: Compass bearing starts at 0 degrees North and increases clockwise.
-
-    :param heading: Heading in radians (0 rad at East, increasing counter-clockwise)
-
-    :return bearing: Compass bearing in degrees (0 deg at North, increasing clockwise)
-    '''
-    x = math.cos(heading)
-    y = math.sin(heading)
-    bearing = numpy.rad2deg(math.atan2(x, y))
-    if bearing < 0: # If atan2 returns a negative bearing
-        bearing += 360
-    return bearing
-
-def bearing_to_heading(bearing):
-    '''
-    Convert given compass bearing in degrees to heading in radians.
-    Note: Compass bearing starts at 0 degrees North and increases clockwise.
-
-    :param bearing: Compass bearing in degrees (0 deg at North, increasing clockwise)
-
-    :return heading: Heading in radians (0 rad at East, increasing counter-clockwise)
-    '''
-    bearing = numpy.deg2rad(bearing)
-    x = math.cos(bearing)
-    y = math.sin(bearing)
-    heading = math.atan2(x, y)
-    if heading < 0: # If atan2 returns a negative heading
-        heading += 2 * math.pi
-    return heading
-
-def local_to_global_tf(x_ref, y_ref, theta_ref, x_loc, y_loc, theta_loc):
-    '''
-    Transform pose of target object (described w.r.t. reference object) to global frame.
-
-    :param x_ref    : X position of reference object w.r.t. global frame (i.e. global posotion)
-    :param y_ref    : Y position of reference object w.r.t. global frame (i.e. global posotion)
-    :param theta_ref: Orientation of reference object w.r.t. global frame (i.e. global posotion)
-    :param x_loc    : X position of target object w.r.t. reference object (i.e. local posotion)
-    :param y_loc    : Y position of target object w.r.t. reference object (i.e. local posotion)
-    :param theta_loc: Orientation of target object w.r.t. reference object (i.e. local posotion)
-
-    :return x_global, y_global, theta_global: Pose of target object w.r.t. global frame (i.e. global pose)
-    '''
-    ref_obj_glb_tf = numpy.array([[math.cos(theta_ref), -math.sin(theta_ref), x_ref],
-                                  [math.sin(theta_ref), math.cos(theta_ref),  y_ref],
-                                  [0,                   0,                    1    ]])
-    tgt_obj_loc_tf = numpy.array([[x_loc],
-                                  [y_loc],
-                                  [1    ]])
-    tgt_obj_glb_tf = numpy.matmul(ref_obj_glb_tf, tgt_obj_loc_tf)
-    x_global, y_global, theta_global = tgt_obj_glb_tf[0][0], tgt_obj_glb_tf[1][0], normalize_angle(theta_ref + theta_loc)
-    return x_global, y_global, theta_global
